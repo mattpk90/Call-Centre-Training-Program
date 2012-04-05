24ecbf67d104fcc6d6936ca9f1b55478cc86f22f
Aftiii <robbie.aftab@gmail.com>
2012-04-05 23:14:44 +0200

    I've made it so we can ascertain which database connection to use. This is done by attempting to use the University connection and if it fails it sends false to a boolean value, this boolean value is then used in getConnection to see which connection string to use. This means we don't have to have two .jar files at the end.

--- Call_Centre_Training.java
+++ Call_Centre_Training.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 
 public class Call_Centre_Training 
 {
+	static boolean inUniversity;
+	
     public static void main(String[] args) throws Exception
     {
         // Set System L&F
@@ -32,18 +34,44 @@ public class Call_Centre_Training
 		
 		CallLogView c = new CallLogView();
 		c.createAndShowGUI();
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://edward.solent.ac.uk:3306/ssd1?user=iaftab&password=rUg9ne4P");
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try
+		{
+			stmt = conn.prepareStatement("SELECT count(*) FROM customer");
+			rs = stmt.executeQuery();
+			if(rs.next())
+			{
+				boolean inUniversity = true;
+			}else
+			{
+				boolean inUniversity = false;
+			}
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
     }
     
      //This is the class which is called for every database connection
      public static Connection getConnection() 
      {
 		Connection conn = null;
+	
 		
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://instance10055.db.xeround.com:6860/bgTraining?user=bgAdmin&password=superuser");
-			//conn = DriverManager.getConnection("jdbc:mysql://edward.solent.ac.uk:3306/ssd1?user=iaftab&password=rUg9ne4P");
+			if(inUniversity)
+			{
+				conn = DriverManager.getConnection("jdbc:mysql://edward.solent.ac.uk:3306/ssd1?user=iaftab&password=rUg9ne4P");
+			}else
+			{
+				conn = DriverManager.getConnection("jdbc:mysql://instance10055.db.xeround.com:6860/bgTraining?user=bgAdmin&password=superuser");
+			}
 		}
 		catch(SQLException ex)
 		{
